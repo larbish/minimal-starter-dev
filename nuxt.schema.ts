e301fca3133b68405710b2733c0662666163372d
Baptiste Leproux <leproux.baptiste@gmail.com>
2023-10-16 15:59:50 +0200

    up

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -40,7 +40,7 @@ export default defineNuxtSchema({
     footer: {
       /**
        * Hide or display the footer
-       * @studioIcon i-mdi-page-layout-he
+       * @studioIcon i-mdi-page-layout-footer
        */
       display: true
     }
